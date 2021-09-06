0d796aaa2cd3d7055434db5506ac80a31110844b
Diego Cantelli <diegocantelli19@gmail.com>
2021-09-06 18:22:33 -0300

    Utilizando o Include para trazer dados de outras entidades relacionadas

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Curso.Data;
 using Curso.Domain;
@@ -14,9 +15,53 @@ namespace CursoEFCore
             // db.Database.Migrate();
             // var exists = db.Database.GetPendingMigrations().Any(); 
             // InserirDados();
-            InserirDadosEmMassa();
+            // InserirDadosEmMassa();
+            // CadastrarPedido();
+            ConsultarPedidoCarregamentoAdiantado();
         }
 
+        private static void ConsultarPedidoCarregamentoAdiantado()
+        {
+            using var db = new ApplicationContext();
+
+            // Com o include, o pedido irá trazer os dados referentes aos itens desse pedido de forma adiantada
+            var pedidos = db.Pedidos
+                .Include(x => x.Itens)
+                    // Com o ThenInclude, os produtos dentro dos itens do pedido serão carregados
+                    .ThenInclude(x => x.Produto)
+                .ToList();
+            Console.WriteLine(pedidos.Count());
+        }
+        private static void CadastrarPedido()
+        {
+            using var db = new ApplicationContext();
+
+            var cliente = db.Clientes.FirstOrDefault();
+            var produto = db.Produtos.FirstOrDefault();
+
+            var pedido = new Pedido
+            {
+                ClienteId = cliente.Id,
+                IniciadoEm = DateTime.Now,
+                FinalizadoEm = DateTime.Now,
+                Observacao = "Pedido Teste",
+                StatusPedido = StatusPedido.Analise,
+                TipoFrete = TipoFrete.SemFrete,
+                Itens = new List<PedidoItem>
+                {
+                    new PedidoItem
+                    {
+                        ProdutoId = produto.Id,
+                        Desconto = 0,
+                        Quantidade = 1,
+                        Valor = 10
+                    }
+                }
+            };
+
+            db.Pedidos.Add(pedido);
+            db.SaveChanges();
+        }
         private static void ConsultarDados()
         {
             using var db = new ApplicationContext();
