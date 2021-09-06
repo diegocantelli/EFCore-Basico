8eb182b3774fd264d468280aedb90e9321c7717a
Diego Cantelli <diegocantelli19@gmail.com>
2021-09-06 09:48:43 -0300

    Aplicando o modelo da migração no BD: dotnet ef database update -p .\urso\CursoEFCore.csproj -v

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -11,14 +11,14 @@ namespace Curso.Data
         // public DbSet<Pedido> Pedidos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data source=(localdb)\\mssqllocaldb: Initial Catalog=CursoEFCore; Integrated Security=true");
+            optionsBuilder.UseSqlServer("Data source=(localdb)\\mssqllocaldb;Initial Catalog=CursoEFCore; Integrated Security=true");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
            // aplicando as configurações definidas em ClienteConfiguration para criar a tabela 
         //    modelBuilder.ApplyConfiguration(new ClienteConfiguration());
-        
+
             // irá aplicar as configurações com base nas classes quem implementem IEntityTypeConfiguration
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
         }
